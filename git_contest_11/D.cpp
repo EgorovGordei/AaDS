1db7f408b0394823fe8ac909e43889fa3fc1f89c
EgorovGordei <78762468+EgorovGordei@users.noreply.github.com>
2021-10-22 19:24:15 +0300

    Changed codestyle D and G

--- git_contest_11/D.cpp
+++ git_contest_11/D.cpp
@@ -11,227 +11,231 @@ class Trie
 private:
     struct Node
     {
-	    int term = 0;
-    	int par = 0;
-	    int symbol = 0;
-    	std::vector<int> edge;
+        int term = 0;
+        int par = 0;
+        int symbol = 0;
+        std::vector<int> edge;
 
-	    Node(int ALPHABET_SIZE, int symbol, int parent) : par(parent), symbol(symbol), edge(ALPHABET_SIZE, -1) {}
+        Node(int ALPHABET_SIZE, int symbol, int parent) : par(parent), symbol(symbol), edge(ALPHABET_SIZE, -1) {}
     };
 
     const int ALPHABET_SIZE = 26;
-	const int STARTING_SYMBOL = 'a';
-	std::vector<Node> trie;
+    const int STARTING_SYMBOL = 'a';
+    std::vector<Node> trie;
 
 public:
-   	struct iterator
-	{
-		int index = -1;
-		int n = 1;
-		Trie* tr;
+    struct iterator
+    {
+        int index = -1;
+        int n = 1;
+        Trie* tr;
 
-		iterator(Trie* tr, int node) : index(node), tr(tr) {}
-		
-		iterator& operator++()
-		{
-			if (n < tr->trie[index].term)
-			{
-				++n;
-				return *this;
-			}
-			n = 1;
-			int current_node = index;
-			bool first = true;
-			int symbol = 0;
-			while (true)
-			{
-				if (current_node == -1)
-				{
-					current_node = tr->trie.size();
-					break;
-				}
-				if (!first && tr->trie[current_node].term > 0)
-				{
-					break;
-				}
-				first = false;
+        iterator(Trie* tr, int node) : index(node), tr(tr) {}
 
-				int next_node = -1;
-				for (int i = symbol; i < int(tr->trie[current_node].edge.size()); ++i)
-				{
-					if (tr->trie[current_node].edge[i] != -1)
-					{
-						next_node = tr->trie[current_node].edge[i];
-						break;
-					}
-				}
-				symbol = 0;
-				if (next_node != -1)
-				{
-					current_node = next_node;
-					continue;
-				}
-	
-				symbol = tr->trie[current_node].symbol - tr->STARTING_SYMBOL + 1;
-				current_node = tr->trie[current_node].par;
-				first = true;
-			}
-			index = current_node;
-			return *this;
-		}
+        int _go_up(int current_node, int symbol)
+        {
+            int next_node = -1;
+            for (int i = symbol; i < int(tr->trie[current_node].edge.size()); ++i)
+            {
+                if (tr->trie[current_node].edge[i] != -1)
+                {
+                    next_node = tr->trie[current_node].edge[i];
+                    break;
+                }
+            }
+            return next_node;
+        }
+ 
+        iterator& operator++()
+        {
+            if (n < tr->trie[index].term)
+            {
+                ++n;
+                return *this;
+            }
+            n = 1;
+            int current_node = index;
+            bool first = true;
+            int symbol = 0;
+            while (true)
+            {
+                if (current_node == -1)
+                {
+                    current_node = tr->trie.size();
+                    break;
+                }
+                if (!first && tr->trie[current_node].term > 0) break;
+                first = false;
 
-		iterator operator++(int)
-		{
-			iterator it(*this);
-			++it;
-			return it;
-		}
+                int next_node = _go_up(current_node, symbol);
+                
+                if (next_node != -1)
+                {
+                    symbol = 0;
+                    current_node = next_node;
+                    continue;
+                }
 
-		int operator*()
-		{
-			return index;
-		}
+                symbol = tr->trie[current_node].symbol - tr->STARTING_SYMBOL + 1;
+                current_node = tr->trie[current_node].par;
+                first = true;
+            }
+            index = current_node;
+            return *this;
+        }
 
-		bool operator==(const iterator& it2) const
-		{
-			return index == it2.index && n == it2.n;
-		}
+        iterator operator++(int)
+        {
+            iterator it(*this);
+            ++it;
+            return it;
+        }
 
-		bool operator!=(const iterator& it2) const
-		{
-			return !(*this == it2);
-		}
+        int operator*()
+        {
+            return index;
+        }
 
-	};
+        bool operator==(const iterator& it2) const
+        {
+            return index == it2.index && n == it2.n;
+        }
 
-	Trie() : trie({ Node(ALPHABET_SIZE, -1, -1) }) {}
+        bool operator!=(const iterator& it2) const
+        {
+            return !(*this == it2);
+        }
 
-	void add_word(const std::string& S)
-	{
-		int current_node = 0;
-		for (const char& c : S)
-		{
-			if (trie[current_node].edge[c - STARTING_SYMBOL] != -1)
-			{
-				current_node = trie[current_node].edge[c - STARTING_SYMBOL];
-			}
-			else
-			{
-				trie.emplace_back(ALPHABET_SIZE, c, current_node);
-				trie[current_node].edge[c - STARTING_SYMBOL] = trie.size() - 1;
-				current_node = trie.size() - 1;
-			}
-		}
-		trie[current_node].term += 1;
-	}
+    };
 
-	int find_word(const std::string& S) const
-	{
-		int current_node = 0;
-		for (const char& c : S)
-		{
-			if (trie[current_node].edge[c - STARTING_SYMBOL] == -1) return 0;
-			current_node = trie[current_node].edge[c - STARTING_SYMBOL];
-		}
-		return (trie[current_node].term > 0) ? current_node : 0;
-	}
+    Trie() : trie({ Node(ALPHABET_SIZE, -1, -1) }) {}
 
-	bool remove_word(const std::string& S)
-	{
-		int current_node = find_word(S);
-		if (current_node == 0) return false;
+    void add_word(const std::string& word)
+    {
+        int current_node = 0;
+        for (const char& letter : word)
+        {
+            if (trie[current_node].edge[letter - STARTING_SYMBOL] != -1)
+            {
+                current_node = trie[current_node].edge[letter - STARTING_SYMBOL];
+            }
+            else
+            {
+                trie.emplace_back(ALPHABET_SIZE, letter, current_node);
+                trie[current_node].edge[letter - STARTING_SYMBOL] = trie.size() - 1;
+                current_node = trie.size() - 1;
+            }
+        }
+        trie[current_node].term += 1;
+    }
 
-		trie[current_node].term -= 1;
-		return true;
-	}
+    int find_word(const std::string& S) const
+    {
+        int current_node = 0;
+        for (const char& c : S)
+        {
+            if (trie[current_node].edge[c - STARTING_SYMBOL] == -1) return 0;
+            current_node = trie[current_node].edge[c - STARTING_SYMBOL];
+        }
+        return (trie[current_node].term > 0) ? current_node : 0;
+    }
 
-	std::string get_word_from_index(int i) const
-	{
-		std::string word = "";
-		while (trie[i].par != -1)
-		{
-			word += trie[i].symbol;
-			i = trie[i].par;
-		}
-		std::reverse(word.begin(), word.end());
-		return word;
-	}
+    bool remove_word(const std::string& S)
+    {
+        int current_node = find_word(S);
+        if (current_node == 0) return false;
 
-	iterator begin()
-	{
-		return iterator(this, 0)++;
-	}
+        trie[current_node].term -= 1;
+        return true;
+    }
 
-	iterator end()
-	{
-		return iterator(this, trie.size());
-	}
+    std::string get_word_from_index(int i) const
+    {
+        std::string word = "";
+        while (trie[i].par != -1)
+        {
+            word += trie[i].symbol;
+            i = trie[i].par;
+        }
+        std::reverse(word.begin(), word.end());
+        return word;
+    }
 
-	std::ostream& out(std::ostream& os) const
-	{
-		std::string S;
-		for (int i = 0; i < int(trie.size()); ++i)
-		{
-			os << i << ":" << "{" << trie[i].term << "," << trie[i].par << "," << "{";
-			for (int j = 0; j < int(trie[i].edge.size()); ++j)
-				if (trie[i].edge[j] != -1)
-					os << j << ":" << trie[i].edge[j] << ",";
-			os << "}}" << "\n";
-		}
-		return os;
-	}
+    iterator begin()
+    {
+        return iterator(this, 0)++;
+    }
+
+    iterator end()
+    {
+        return iterator(this, trie.size());
+    }
+
+    std::ostream& out(std::ostream& os) const
+    {
+        std::string S;
+        for (int i = 0; i < int(trie.size()); ++i)
+        {
+            os << i << ":" << "{" << trie[i].term << "," << trie[i].par << "," << "{";
+            for (int j = 0; j < int(trie[i].edge.size()); ++j)
+                if (trie[i].edge[j] != -1)
+                    os << j << ":" << trie[i].edge[j] << ",";
+            os << "}}" << "\n";
+        }
+        return os;
+    }
 };
 
 std::ostream& operator<<(std::ostream& os, const Trie& t)
 {
-	return t.out(os);
+    return t.out(os);
 }
 
 std::string solve(const std::string& S)
 {
     std::vector<int> dots(1, 0);
-	std::string word;
-	Trie t;
-	for (auto c : S)
-	{
-		if (c == '.')
-		{
-			if (word != "")
-			{
-				t.add_word(word);
-				word = "";
-				dots.push_back(1);
-			}
-			else
-			{
-				dots[dots.size() - 1] += 1;
-			}
-		}
-		else
-		{
-			word += c;
-		}
-	}
-	if (word != "") t.add_word(word);
+    std::string word;
+    Trie t;
+    for (auto c : S)
+    {
+        if (c == '.')
+        {
+            if (word != "")
+            {
+                t.add_word(word);
+                word = "";
+                dots.push_back(1);
+            }
+            else
+            {
+                dots[dots.size() - 1] += 1;
+            }
+        }
+        else
+        {
+            word += c;
+        }
+    }
+    if (word != "") t.add_word(word);
 
     std::string ans;
-	int j = 0;
-	for (auto it = t.begin(); it != t.end(); ++it)
-	{
-		for (int i = 0; i < dots[j]; ++i) ans += ".";
-		ans += t.get_word_from_index(*it);
-		j += 1;
-	}
-	if (int(dots.size()) > j) for (int i = 0; i < dots[j]; ++i) ans += ".";
+    int j = 0;
+    for (auto it = t.begin(); it != t.end(); ++it)
+    {
+        for (int i = 0; i < dots[j]; ++i) ans += ".";
+        ans += t.get_word_from_index(*it);
+        j += 1;
+    }
+    if (int(dots.size()) > j) for (int i = 0; i < dots[j]; ++i) ans += ".";
 
     return ans;
 }
 
 int main()
 {
-	std::string S;
-	std::cin >> S;
+    std::string S;
+    std::cin >> S;
 
-	std::cout << solve(S);
+    std::cout << solve(S);
 }
 
--- git_contest_11/G.cpp
+++ git_contest_11/G.cpp
@@ -9,109 +9,138 @@ class Trie
 private:
     struct Node
     {
+        static const int ALPHABET_SIZE = 2; 
         std::vector<int32_t> indexes;
-    	int32_t to[26];
+        int32_t to[ALPHABET_SIZE];
         int32_t height = 0;
-    	int32_t link = -1;
+        int32_t link = -1;
         bool mark = false;
 
-	    Node(int height) : height(height), link(-1) 
-    	{
-	    	for (int i = 0; i < 26; ++i) to[i] = -1;
-	    }
+        Node(int height) : height(height), link(-1) 
+        {
+            for (int i = 0; i < ALPHABET_SIZE; ++i) to[i] = -1;
+        }
     };
 
-	const int ALPHABET_SIZE = 2;
-	const int STARTING_SYMBOL = '0';
-	std::vector<Node> trie;
+    const int STARTING_SYMBOL = '0';
+    std::vector<Node> trie;
 
 public:
-	Trie() : trie({ Node(0) }) {}
+    Trie() : trie({ Node(0) }) {}
 
-	void add_word(const std::string& S, int ind)
-	{
-		int current_node = 0;
-		for (const char& c : S)
-		{
-			if (trie[current_node].to[c - STARTING_SYMBOL] != -1)
-			{
-				current_node = trie[current_node].to[c - STARTING_SYMBOL];
-			}
-			else
-			{
+    void add_word(const std::string& S, int ind)
+    {
+        int current_node = 0;
+        for (const char& c : S)
+        {
+            if (trie[current_node].to[c - STARTING_SYMBOL] != -1)
+            {
+                current_node = trie[current_node].to[c - STARTING_SYMBOL];
+            }
+            else
+            {
                 trie.emplace_back(trie[current_node].height + 1);
-				trie[current_node].to[c - STARTING_SYMBOL] = trie.size() - 1;
-				current_node = trie.size() - 1;
-			}
-		}
-		trie[current_node].indexes.push_back(ind);
+                trie[current_node].to[c - STARTING_SYMBOL] = trie.size() - 1;
+                current_node = trie.size() - 1;
+            }
+        }
+        trie[current_node].indexes.push_back(ind);
     }
 
-	void aho_corasick()
-	{
-		int current_node = 0;
-		trie[current_node].link = 0;
-		for (int i = 0; i < ALPHABET_SIZE; ++i)
-		{
-			if (trie[current_node].to[i] == -1)
-				trie[current_node].to[i] = 0;
-		}
+    void aho_corasick()
+    {
+        int current_node = 0;
+        trie[current_node].link = 0;
+        for (int i = 0; i < Trie::Node::ALPHABET_SIZE; ++i)
+        {
+            if (trie[current_node].to[i] == -1)
+                trie[current_node].to[i] = 0;
+        }
 
-		std::queue<int> processed_nodes;
-		processed_nodes.push(current_node);
-		while(processed_nodes.size() > 0)
-		{
-			current_node = processed_nodes.front();
-			processed_nodes.pop();
-			for (int i = 0; i < ALPHABET_SIZE; ++i)
-			{
-				int child = trie[current_node].to[i];
-				if (child == -1 || trie[child].link != -1) continue;
-				
-				if (current_node == 0)
-					trie[child].link = 0;
-				else
-					trie[child].link = trie[trie[current_node].link].to[i];
-				
-				for (int j = 0; j < ALPHABET_SIZE; ++j)
-				{
-					if (trie[child].to[j] == -1)
-						trie[child].to[j] = trie[trie[child].link].to[j];
-				}
+        std::queue<int> processed_nodes;
+        processed_nodes.push(current_node);
+        while(processed_nodes.size() > 0)
+        {
+            current_node = processed_nodes.front();
+            processed_nodes.pop();
+            for (int i = 0; i < Trie::Node::ALPHABET_SIZE; ++i)
+            {
+                int child = trie[current_node].to[i];
+                if (child == -1 || trie[child].link != -1) continue;
+                
+                if (current_node == 0)
+                    trie[child].link = 0;
+                else
+                    trie[child].link = trie[trie[current_node].link].to[i];
+                
+                for (int j = 0; j < Trie::Node::ALPHABET_SIZE; ++j)
+                {
+                    if (trie[child].to[j] == -1)
+                        trie[child].to[j] = trie[trie[child].link].to[j];
+                }
 
-				processed_nodes.push(child);
-			}
-		}
-	}
+                processed_nodes.push(child);
+            }
+        }
+    }
 
-	const Node& operator[](int i) const
-	{
-		return trie[i];
-	}
+    const Node& operator[](int i) const
+    {
+        return trie[i];
+    }
 
-	Node& operator[](int i)
-	{
-		return trie[i];
-	}
+    Node& operator[](int i)
+    {
+        return trie[i];
+    }
 
-	std::ostream& out(std::ostream& os) const
-	{
-		std::string S;
-		for (int i = 0; i < int(trie.size()); ++i)
-		{
-			os << i << ":" << "{" << trie[i].height << ",";
-			os << trie[i].link << "," << "{";
-			for (int j = 0; j < ALPHABET_SIZE; ++j)
-				if (trie[i].to[j] > 0)
-					os << j << ":" << trie[i].to[j] << ",";
-			os << "}}" << "\n";
-		}
-		return os;
-	}
+    std::ostream& out(std::ostream& os) const
+    {
+        std::string S;
+        for (int i = 0; i < int(trie.size()); ++i)
+        {
+            os << i << ":" << "{" << trie[i].height << ",";
+            os << trie[i].link << "," << "{";
+            for (int j = 0; j < Trie::Node::ALPHABET_SIZE; ++j)
+                if (trie[i].to[j] > 0)
+                    os << j << ":" << trie[i].to[j] << ",";
+            os << "}}" << "\n";
+        }
+        return os;
+    }
 
     int size() const
     {
         return trie.size();
+    }
+
+    int _dfs()
+    {
+        // dfs
+        std::vector<std::pair<int, int>> stack;
+        stack.push_back({0,0});
+        std::vector<int> used = std::vector<int>(trie.size(), 0);
+        while (stack.size() > 0)
+        {
+            int cur_node = stack.back().first;
+            int cur_lett = stack.back().second;
+            if (cur_lett == Trie::Node::ALPHABET_SIZE)
+            {
+                used[cur_node] = 2;
+                stack.pop_back();
+                continue;
+            }
+            used[cur_node] = 1;
+            stack.back().second += 1;
+            cur_lett = stack.back().second;
+            int node_to = trie[cur_node].to[cur_lett-1];
+            if (trie[node_to].mark) continue;
+            int color = used[node_to];
+            if (color == 1) return true;
+            if (color == 2) continue;
+            stack.push_back(std::make_pair(node_to,0));
+        }
+        return false;
     }
 
     int is_infinite()
@@ -128,40 +157,21 @@ public:
                 continue;
             }
             used[q.front()] = 1;
-            for (int i = 0; i < ALPHABET_SIZE; ++i)
+            for (int i = 0; i < Trie::Node::ALPHABET_SIZE; ++i)
             {
                 if (used[trie[q.front()].to[i]] == 0) q.push(trie[q.front()].to[i]);
             }
             if (trie[trie[q.front()].link].mark || trie[q.front()].indexes.size() > 0) trie[q.front()].mark = true;
             q.pop();
         }
-        // dfs
-        std::vector<std::pair<int, int>> stack;
-        stack.push_back({0,0});
-        used = std::vector<int>(trie.size(), 0);
-        while (stack.size() > 0)
-        {
-            if (stack.back().second == ALPHABET_SIZE)
-            {
-                used[stack.back().first] = 2;
-                stack.pop_back();
-                continue;
-            }
-            used[stack.back().first] = 1;
-            stack.back().second += 1;
-            if (trie[trie[stack.back().first].to[stack.back().second-1]].mark) continue;
-            if (used[trie[stack.back().first].to[stack.back().second-1]] == 1) return true;
-            if (used[trie[stack.back().first].to[stack.back().second-1]] == 2) continue;
-            stack.push_back(std::make_pair(trie[stack.back().first].to[stack.back().second-1],0));
-        }
 
-        return false;
+        return _dfs();
     }
 };
 
 std::ostream& operator<<(std::ostream& os, const Trie& t)
 {
-	return t.out(os);
+    return t.out(os);
 }
 
 int main()
@@ -171,13 +181,13 @@ int main()
 
    std::cin >> N;
     for (int i = 0; i < N; ++i)
-	{
-		std::string T;
-		std::cin >> T;
+    {
+        std::string T;
+        std::cin >> T;
         t.add_word(T, i);
-	}
+    }
 
-	t.aho_corasick();
+    t.aho_corasick();
     //std::cout << t;
     if (t.is_infinite())
         std::cout << "TAK";
